c77def2b1c0f101cb50c2313acf184f26ec1dc4b
Todd Sundsted <todd@sundsted.com>
2012-03-16 15:38:58 -0400

    Added an early early version of a browser widget based on jquery ui.

--- files/public/js/moo-0.0.0.js
+++ files/public/js/moo-0.0.0.js
@@ -4,22 +4,67 @@ var Moo = {};
 
 (function(Moo) {
 
+  /* utilities */
+
+  var endsWith = function(string, suffix) {
+    return string.indexOf(suffix, string.length - suffix.length) !== -1;
+  };
+
+  /* monkey patches / helpers */
+
+  Number.prototype.toHTML = function() {
+    return '<span class="moo-num">' + this + '</span>';
+  };
+
+  String.prototype.toHTML = function() {
+    var str, num;
+    if (endsWith(this, '|err')) {
+      str = _.escape(this.slice(0, this.length - 4));
+      return '<span class="moo-err">' + str + '</span>';
+    }
+    else if (endsWith(this, '|obj')) {
+      str = _.escape(this.slice(0, this.length - 4));
+      num = _.escape(this.slice(0, this.length - 4).slice(1));
+      return '<a rel="object" data-object-number="' + num + '" class="moo-obj" href="/objects/' + num + '">' + str + '</a>';
+    }
+    else if (endsWith(this, '|int')) {
+      str = _.escape(this.slice(0, this.length - 4));
+      return '<span class="moo-num">' +  str + '</span>';
+    }
+    else if (endsWith(this, '|float')) {
+      str = _.escape(this.slice(0, this.length - 6));
+      return '<span class="moo-num">' +  str + '</span>';
+    }
+    else if (endsWith(this, '|str')) {
+      str = _.escape(this.slice(0, this.length - 4));
+      return '<span class="moo-str">"' +  str + '"</span>';
+    }
+    else {
+      str = _.escape(this);
+      return '<span class="moo-str">"' + str + '"</span>';
+    }
+  };
+
+  Array.prototype.toHTML = function() {
+    return '<span class="moo-list">{ ' + _.map(this, function(v) { return v.toHTML(); }).join(', ') + ' }</span>';
+  };
+
+  Object.prototype.toHTML = function() {
+    return '<span class="moo-map">[ ' + _.map(this, function(v, k) { return k.toHTML() + ' -> ' + v.toHTML(); }).join(', ') + ' ]</span>';
+  };
+
   _.extend(Moo, {
 
-    endsWith: function(string, suffix) {
-      return string.indexOf(suffix, string.length - suffix.length) !== -1;
-    },
-
     isString: function(value) {
-      return typeof(value) == 'string' && !(Moo.endsWith(value, '|obj') || Moo.endsWith(value, '|err'));
+      return typeof(value) == 'string' && !(endsWith(value, '|obj') || endsWith(value, '|err'));
     },
 
     isObjectNumber: function(value) {
-      return typeof(value) == 'string' && Moo.endsWith(value, '|obj');
+      return typeof(value) == 'string' && endsWith(value, '|obj');
     },
 
     isError: function(value) {
-      return typeof(value) == 'string' && Moo.endsWith(value, '|err');
+      return typeof(value) == 'string' && endsWith(value, '|err');
     },
 
     isBinary: function(value) {
@@ -53,6 +98,17 @@ var Moo = {};
 
   Moo.NestedModel = Backbone.Model.extend({
 
+    initialize: function(attributes) {
+      /* remove `toHTML' from attributes */
+      /* it is a side-effect caused by adding `toHTML()' to `Object' */
+      delete this.attributes.toHTML;
+    },
+
+    has: function(attr) {
+      var result = this.get(attr);
+      return !(result === null || result === undefined);
+    },
+
     get: function(attr, opts) {
       opts = opts || {};
 
@@ -62,7 +118,7 @@ var Moo = {};
       var value = Backbone.Model.prototype.get.call(this, root, opts);
 
       _.each(path, function(p) {
-        value = value[p];
+        value = value != undefined && (p in value) ? value[p] : undefined;
       });
 
       return value;
@@ -252,6 +308,31 @@ var Moo = {};
       return response;
     },
 
+    fetch: function(options) {
+      options = options ? _.clone(options) : {};
+
+      var model = this;
+      var error = options.error;
+      options.error = function(model, resp) {
+        model.httpResponseStatus = resp.status;
+        if (error) error(model, resp);
+      };
+
+      return Backbone.Model.prototype.fetch.call(this, options);
+    },
+
+    failed: function() {
+      return this.httpResponseStatus >= 300;
+    },
+
+    isNotFound: function() {
+      return this.httpResponseStatus == 404;
+    },
+
+    isDenied: function() {
+      return this.httpResponseStatus == 403;
+    },
+
     toJSON: function() {
       var to_map = function(a, v, k) {
         a[v.id] = v;
@@ -271,6 +352,94 @@ var Moo = {};
   Moo.Objects = Backbone.Collection.extend({
     model: Moo.Object,
     url: '///objects'
+  });
+
+  $.widget('moo.simpleObjectPanel', {
+
+    options: {
+      object: null,
+      template: '<div class="row">' +
+          '<div class="span12">' +
+            '<h1>' +
+              '<% if (object.isNotFound()) { %>' +
+                '<em>Not Found</em>' +
+              '<% } else if (object.isDenied()) { %>' +
+                '<em>Request Denied</em>' +
+              '<% } else if (object.failed()) { %>' +
+                '<em>Request Failed</em>' +
+              '<% } else { %>' +
+                '<%= object.values.get("name").get("Value.value") %>' +
+              '<% } %>' +
+              '<% if ((p = object.attributez.get("player")) && (p = p.get("Value.value"))) { %> ' +
+                '<small class="player-flag">player</small>' +
+              '<% } %>' +
+              '<% if ((p = object.values.get("programmer")) && (p = p.get("Value.value"))) { %> ' +
+                '<small class="programmer-flag">programmer</small>' +
+              '<% } %>' +
+              '<% if ((p = object.values.get("wizard")) && (p = p.get("Value.value"))) { %> ' +
+                '<small class="wizard-flag">wizard</small>' +
+              '<% } %>' +
+              '<% if ((v = object.values.get("r")) && (v = v.get("Value.value"))) { %> ' +
+                '<small class="read-flag">R</small>' +
+              '<% } %>' +
+              '<% if ((v = object.values.get("w")) && (v = v.get("Value.value"))) { %> ' +
+                '<small class="write-flag">W</small>' +
+              '<% } %>' +
+              '<% if ((v = object.values.get("f")) && (v = v.get("Value.value"))) { %> ' +
+                '<small class="fertile-flag">F</small>' +
+              '<% } %>' +
+              '<% if ((p = object.attributez.get("parents")) && (p = p.get("Value.value")) && p.length > 0) { %> ' +
+                ' <small class="parents"><%= p.toHTML() %></small>' +
+              '<% } %>' +
+            '</h1>' +
+          '</div>' +
+          '<div class="span4">' +
+            '<h2>Properties</h2>' +
+            '<table class="table table-condensed">' +
+              '<% _.each(object.values.models, function(value) {' +
+                'var v = value.get("Value.value"); %>' +
+                '<tr><td><%= value.get("id") %></td><td><%= v !== undefined ? v.toHTML() : "---" %></td></tr>' +
+              '<% }); %>' +
+            '</table>' +
+          '</div>' +
+          '<div class="span4">' +
+            '<h2>Property Definitions</h2>' +
+          '</div>' +
+          '<div class="span4">' +
+            '<h2>Verb Definitions</h2>' +
+          '</div>' +
+        '</div>'
+    },
+
+    _setOption: function(key, value) {
+      switch (key) {
+        case 'object':
+          this.options.object = value;
+          this._jamba();
+          break;
+        case 'template':
+          this.options.template = value;
+          this._jamba();
+          break;
+      }
+    },
+
+    _jamba: function() {
+      if (this.options.object) {
+        var template = _.template(this.options.template);
+        this.element.html(template({
+          object: this.options.object
+        }));
+      }
+    },
+
+    _create: function() {
+      this.options.originalContent = this.element.html();
+    },
+
+    destroy: function() {
+      this.element.html(this.options.originalContent);
+    }
   });
 
 })(Moo);
--- files/public/spec/MooSpec.js
+++ files/public/spec/MooSpec.js
@@ -1,4 +1,84 @@
-/* global Moo describe it expect beforeEach afterEach */
+/* global _ $ Moo describe it expect beforeEach afterEach spyOn */
+
+describe('Number', function() {
+  it('should HTML-ize a number', function() {
+    var n = 123;
+    var $v = $(n.toHTML());
+    expect($v.hasClass('moo-num')).toBeTruthy();
+    expect($v.text()).toEqual('123');
+  });
+});
+
+describe('String', function() {
+  it('should HTML-ize a MOO error', function() {
+    var $v = $('E_FOO|err'.toHTML());
+    expect($v.hasClass('moo-err')).toBeTruthy();
+    expect($v.text()).toEqual('E_FOO');
+  });
+
+  it('should HTML-ize a MOO object number', function() {
+    var $v = $('#0|obj'.toHTML());
+    expect($v.hasClass('moo-obj')).toBeTruthy();
+    expect($v.attr('href')).toEqual('/objects/0');
+    expect($v.text()).toEqual('#0');
+  });
+
+  it('should HTML-ize a MOO integer', function() {
+    var $v = $('123|int'.toHTML());
+    expect($v.hasClass('moo-num')).toBeTruthy();
+    expect($v.text()).toEqual('123');
+  });
+
+  it('should HTML-ize a MOO float', function() {
+    var $v = $('1.3|float'.toHTML());
+    expect($v.hasClass('moo-num')).toBeTruthy();
+    expect($v.text()).toEqual('1.3');
+  });
+
+  it('should HTML-ize a tricky MOO string', function() {
+    var $v = $('#0|obj|str'.toHTML());
+    expect($v.hasClass('moo-str')).toBeTruthy();
+    expect($v.text()).toEqual('"#0|obj"');
+  });
+
+  it('should HTML-ize a simple MOO string', function() {
+    var $v = $('str'.toHTML());
+    expect($v.hasClass('moo-str')).toBeTruthy();
+    expect($v.text()).toEqual('"str"');
+  });
+
+  it('should sanitize HTML', function() {
+    var $v = $('<script>alert("bang!");</script>'.toHTML());
+    expect($v.hasClass('moo-str')).toBeTruthy();
+    expect($v.html()).toEqual('"&lt;script&gt;alert("bang!");&lt;/script&gt;"');
+    expect($v.text()).toEqual('"<script>alert("bang!");</script>"');
+  });
+});
+
+describe('Array', function() {
+  it('should HTML-ize an array of values', function() {
+    var a = [123, '123', '#123|obj'];
+    var $v = $(a.toHTML());
+    expect($v.hasClass('moo-list')).toBeTruthy();
+    expect($v.children().length).toEqual(3);
+    expect($v.find(':eq(0)').text()).toEqual('123');
+    expect($v.find(':eq(1)').text()).toEqual('"123"');
+    expect($v.find(':eq(2)').text()).toEqual('#123');
+  });
+});
+
+describe('Object', function() {
+  it('should HTML-ize an object of value pairs', function() {
+    var o = {'123|int': 123, '123|str': '123'};
+    var $v = $(o.toHTML());
+    expect($v.hasClass('moo-map')).toBeTruthy();
+    expect($v.children().length).toEqual(4);
+    expect($v.find(':eq(0)').text()).toEqual('123');
+    expect($v.find(':eq(1)').text()).toEqual('123');
+    expect($v.find(':eq(2)').text()).toEqual('"123"');
+    expect($v.find(':eq(3)').text()).toEqual('"123"');
+  });
+});
 
 describe('Moo.NestedModel', function() {
 
@@ -67,6 +147,7 @@ describe('Moo.NestedModel', function() {
     test.attributes = {foo: 'bar', one: 'two'};
     expect(test.get('foo')).toEqual('bar');
     expect(test.get('one')).toEqual('two');
+    expect(test.get('xyz')).toBeUndefined();
   });
 
   it('should allow singly-nested attributes to be got with dotted notation', function() {
@@ -74,6 +155,7 @@ describe('Moo.NestedModel', function() {
     test.attributes = {foo: {bar: 'baz'}, one: {two: 'three'}};
     expect(test.get('foo.bar')).toEqual('baz');
     expect(test.get('one.two')).toEqual('three');
+    expect(test.get('xyz.abc')).toBeUndefined();
   });
 
   it('should allow doubly-nested attributes to be got with dotted notation', function() {
@@ -81,6 +163,7 @@ describe('Moo.NestedModel', function() {
     test.attributes = {foo: {bar: {baz: 'quux'}}, one: {two: {three: 'four'}}};
     expect(test.get('foo.bar.baz')).toEqual('quux');
     expect(test.get('one.two.three')).toEqual('four');
+    expect(test.get('xyz.abc.mno')).toBeUndefined();
   });
 
   it('should allow overlapping nested attributes to be got with dotted notation', function() {
@@ -88,6 +171,39 @@ describe('Moo.NestedModel', function() {
     test.attributes = {foo: {bar: 1, baz: 2}};
     expect(test.get('foo.bar')).toEqual(1);
     expect(test.get('foo.baz')).toEqual(2);
+    expect(test.get('foo.xyz')).toBeUndefined();
+  });
+
+  it('should allow non-nested attributes to be introspected with dotted notation', function() {
+    var test = new Test;
+    test.attributes = {foo: 'bar', one: 'two'};
+    expect(test.has('foo')).toBeTruthy();
+    expect(test.has('one')).toBeTruthy();
+    expect(test.has('xyz')).toBeFalsy();
+  });
+
+  it('should allow singly-nested attributes to be introspected with dotted notation', function() {
+    var test = new Test;
+    test.attributes = {foo: {bar: 'baz'}, one: {two: 'three'}};
+    expect(test.has('foo.bar')).toBeTruthy();
+    expect(test.has('one.two')).toBeTruthy();
+    expect(test.has('xyz.abc')).toBeFalsy();
+  });
+
+  it('should allow doubly-nested attributes to be introspected with dotted notation', function() {
+    var test = new Test;
+    test.attributes = {foo: {bar: {baz: 'quux'}}, one: {two: {three: 'four'}}};
+    expect(test.has('foo.bar.baz')).toBeTruthy();
+    expect(test.has('one.two.three')).toBeTruthy();
+    expect(test.has('xyz.abc.mno')).toBeFalsy();
+  });
+
+  it('should allow overlapping nested attributes to be introspected with dotted notation', function() {
+    var test = new Test;
+    test.attributes = {foo: {bar: 1, baz: 2}};
+    expect(test.has('foo.bar')).toBeTruthy();
+    expect(test.has('foo.baz')).toBeTruthy();
+    expect(test.has('foo.xyz')).toBeFalsy();
   });
 });
 
@@ -518,4 +634,170 @@ describe('Moo.Object', function() {
     o.values.get('f').set('Value.value', 1);
     expect(o.isValid()).toBeTruthy();
   });
+
+  it('should ensure failed() returns true if fetch() failed for some reason', function() {
+    spyOn($, "ajax").andCallFake(function(options) {
+      options.error({status: 500}, 'error', 'Internal Server Error');
+    });
+    var o = new Moo.Object({id: 1});
+    o.fetch();
+    expect(o.failed()).toBeTruthy();
+  });
+
+  it('should ensure failed() returns false if fetch() did not fail for any reason', function() {
+    spyOn($, "ajax").andCallFake(function(options) {
+      options.success({}, 'Ok', {status: 200});
+    });
+    var o = new Moo.Object({id: 1});
+    o.fetch();
+    expect(o.failed()).toBeFalsy();
+  });
+
+  it('should ensure isDenied() returns true if access to the object with the requested id was denied', function() {
+    spyOn($, "ajax").andCallFake(function(options) {
+      options.error({status: 403}, 'error', 'Forbidden');
+    });
+    var o = new Moo.Object({id: 1});
+    o.fetch();
+    expect(o.isDenied()).toBeTruthy();
+  });
+
+  it('should ensure isDenied() returns false if access to the object with the requested id was not denied', function() {
+    spyOn($, "ajax").andCallFake(function(options) {
+      options.success({}, 'Ok', {status: 200});
+    });
+    var o = new Moo.Object({id: 1});
+    o.fetch();
+    expect(o.isDenied()).toBeFalsy();
+  });
+
+  it('should ensure isNotFound() returns true if the object with the requested id does not exist on the server', function() {
+    spyOn($, "ajax").andCallFake(function(options) {
+      options.error({status: 404}, 'error', 'Not Found');
+    });
+    var o = new Moo.Object({id: 1});
+    o.fetch();
+    expect(o.isNotFound()).toBeTruthy();
+  });
+
+  it('should ensure isNotFound() does not return true if the object with the requested id exists on the server', function() {
+    spyOn($, 'ajax').andCallFake(function(options) {
+      options.success({}, 'Ok', {status: 200});
+    });
+    var o = new Moo.Object({id: 1});
+    o.fetch();
+    expect(o.isNotFound()).toBeFalsy();
+  });
+});
+
+describe('widget $().simpleObjectPanel', function() {
+
+  var o, p;
+
+  beforeEach(function() {
+    p = $('<div></div>').simpleObjectPanel();
+    o = new Moo.Object({
+      Attributes: {},
+      Values: {name: {Value: {value: 'Test'}}},
+      Properties: [],
+      Verbs: []
+    });
+  });
+
+  it('should display the object name in the heading', function() {
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1').text()).toContain('Test');
+  });
+
+  it('should display "Request Failed" in the heading if the request for the object failed', function() {
+    spyOn(o, 'failed').andReturn(1);
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1').text()).toContain('Request Failed');
+  });
+
+  it('should display "Request Denied" in the heading if the request for the object was denied', function() {
+    spyOn(o, 'isDenied').andReturn(1);
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1').text()).toContain('Request Denied');
+  });
+
+  it('should display "Not Found" in the heading if the object does not exist on the server', function() {
+    spyOn(o, 'isNotFound').andReturn(1);
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1').text()).toContain('Not Found');
+  });
+
+  it('should display the player flag in the heading if the object is a player', function() {
+    o.attributez.add({id: 'player', 'Value.value': 1});
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1 > .player-flag').length).toEqual(1);
+  });
+
+  it('should not display the player flag in the heading if the object is not a player', function() {
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1 > .player-flag').length).toEqual(0);
+  });
+
+  it('should display the programmer flag in the heading if the object is a programmer', function() {
+    o.values.add({id: 'programmer', 'Value.value': 1});
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1 > .programmer-flag').length).toEqual(1);
+  });
+
+  it('should not display the programmer flag in the heading if the object is not a programmer', function() {
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1 > .programmer-flag').length).toEqual(0);
+  });
+
+  it('should display the wizard flag in the heading if the object is a wizard', function() {
+    o.values.add({id: 'wizard', 'Value.value': 1});
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1 > .wizard-flag').length).toEqual(1);
+  });
+
+  it('should not display the wizard flag in the heading if the object is not a wizard', function() {
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1 > .wizard-flag').length).toEqual(0);
+  });
+
+  it('should display the read flag in the heading if the object is readable', function() {
+    o.values.add({id: 'r', 'Value.value': 1});
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1 > .read-flag').length).toEqual(1);
+  });
+
+  it('should not display the read flag in the heading if the object is not readable', function() {
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1 > .read-flag').length).toEqual(0);
+  });
+
+  it('should display the write flag in the heading if the object is writeable', function() {
+    o.values.add({id: 'w', 'Value.value': 1});
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1 > .write-flag').length).toEqual(1);
+  });
+
+  it('should not display the write flag in the heading if the object is not writeable', function() {
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1 > .write-flag').length).toEqual(0);
+  });
+
+  it('should display the fertile flag in the heading if the object is fertile', function() {
+    o.values.add({id: 'f', 'Value.value': 1});
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1 > .fertile-flag').length).toEqual(1);
+  });
+
+  it('should not display the fertile flag in the heading if the object is not fertileable', function() {
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1 > .fertile-flag').length).toEqual(0);
+  });
+
+  it('should display the parents in the heading', function() {
+    o.attributez.add({id: 'parents', 'Value.value': ['#100|obj', '#200|obj']});
+    p.simpleObjectPanel({object: o});
+    expect(p.find('h1 > .parents').length).toEqual(1);
+  });
+
+
 });
